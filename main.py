82289901b56ad7e985e22287218fa7eaf9fe0dd0
banenoi <banen20042008@gmail.com>
2025-05-25 07:33:54 +0000

    Coodbook

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
-to_do=[]
-while True :
-  print  ("--- Task Manager ---")
-  add=int(input("1. Add task\n2. View tasks\n3. Mark task as done\n4. Delete task\n5. Exit\n Choose an option:" ))
-  if add==1:
-    task=input("enter the task:")
-    to_do.append(task)
-    print("task added")
-  elif add==2:
-    print("tasks:")
-    for i in range(len(to_do)):
-      print(to_do[i])
-  elif add==3:
-    task=input("enter the task you want to mark as don ")
-    to_do.append(f"*{task} done*")
-    print(f"your to do list :\n {to_do}")
-    
-  elif add==4:
-    task=input("enter the task to delete:")
-    if task in to_do:
-      to_do.remove(task)
-      print(f"task deleted \n your to do list is:\n{to_do}")
-  elif add==5:
-    break
-  else:
-    print("invield choice ")
-print("welcome ")
-    
+library = {
+    "The Alchemist": {"author": "Paulo Coelho", "year": 1988},
+    "1984": {"author": "George Orwell", "year": 1949}
+}
+while True:
+  print("\n📚 Library Menu")
+  print("1. Show all books")
+  print("2. Add a book")
+  print("3. Delete a book")
+  print("4. Search for a book")
+  print("5. Show book details")
+  print("6. Exit")
+  choice = input("Choose an option: ")
+  match choice:
+     case "1":
+       print("\nBooks in the library:")
+       for title, author in library.items():
+          print(f"{title} by {author}")
+     case "2" :
+       add=input("enter a name of book ")
+       author=input("enter a name of author")
+       library[add]= author
+       print(f"{add} by {author}")
+     case "3" :
+       rmov=input("enter the name of a book to remove ")
+       del library[rmov]
+       print("  beleted successfully of ",rmov)
+     case "4":
+       research=input("enter a tittle of book you want to research")
+       for title , author in library.items():
+         if title == research:
+           print(f"{title} by {author}")
+         else:
+           print(f"this a title {research} not found")
+     case "5" :
+       book=input("enter the title to gave you the details")
+       for title , author in library.items():
+          if title == book:
+            print(f" {author}")
+          else:
+           print(f"this a title {book} not found")
+     case _ :
+       print("invalid choice please try again.")
+     case "6":
+       print("good bye ")
+       break 
+            
+     
+
+  
+
+
+  
